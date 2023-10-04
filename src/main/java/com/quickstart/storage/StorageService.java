2766d69bbd6a13f4adc2ccf94cd3e17f163b5785
JW9506 <cf.dota@gmail.com>
2023-10-04 06:09:10 -0500

    Add comments

--- src/main/java/com/quickstart/storage/StorageService.java
+++ src/main/java/com/quickstart/storage/StorageService.java
@@ -7,15 +7,21 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
 
+    // void init() method is used to initialize the storage service.
     void init();
 
+    // store() method is used to store a file.
     void store(MultipartFile file);
 
+    // loadAll() method is used to load all files as a Stream<Path> object.
     Stream<Path> loadAll();
 
+    // load() method is used to load a file.
     Path load(String filename);
 
+    // loadAsResource() method is used to load a file as a Resource object.
     Resource loadAsResource(String filename);
 
+    // deleteAll() method is used to delete all files.
     void deleteAll();
 }
