3f71120716a8f86031a586589721a8b7e59dbade
JW9506 <cf.dota@gmail.com>
2023-10-08 06:40:22 -0500

    Add a post endpoint for file upload, invoking the store api

--- src/main/java/com/quickstart/web/FileUploadController.java
+++ src/main/java/com/quickstart/web/FileUploadController.java
@@ -4,12 +4,34 @@ import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.quickstart.storage.StorageService;
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class FileUploadController {
+
+  private final StorageService storageService;
+
   @GetMapping("/")
   public String listUploadedFiles(Model model) throws IOException {
 
     return "uploadForm";
   }
+
+  @PostMapping("/")
+  public String handleFileUpload(@RequestParam("file") MultipartFile file,
+      RedirectAttributes redirectAttributes) {
+
+    storageService.store(file);
+    redirectAttributes.addFlashAttribute("message",
+        "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+    return "redirect:/";
+  }
+
 }
